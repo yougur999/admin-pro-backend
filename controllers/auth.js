a0e79034f5bc6628ab1ffdf001bd464402b0b160
Sergio Salinas <sergio-salinas@outlook.com>
2021-03-24 19:47:15 +0100

    Fin sección 10 - Auth

---
+++ controllers/auth.js
@@ -0,0 +1,54 @@
+const { response } = require('express');
+const Usuario = require('../models/usuario');
+const bcrypt = require('bcryptjs');
+const { generarJWT } = require('../helpers/jwt');
+
+
+const login = async (req, res = response) => {
+
+    // verificar email
+    const { email, password } = req.body;
+
+
+    try {
+
+        const usuarioDB = await Usuario.findOne({ email });
+
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Email no encontrado'
+            });
+        }
+    
+        // verificar contraseña 
+        const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Contraseña no válida'
+            });
+        }
+    
+        // Generar un token -> JWT 
+        const token = await generarJWT(usuarioDB.id);
+
+
+        res.json({
+            ok: true,
+            token
+        })
+         
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false, 
+            msg: 'Hable con el administrador...'
+        });
+    }
+
+}
+
+module.exports = {
+    login
+}
--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const app = express();
 // Configurar CORS 
 app.use(cors());
 
+// Lectura y parseo del body
+app.use(express.json());
+
 // Base de datos
 dbConnection();
 
@@ -20,12 +23,9 @@ dbConnection();
 
 
 // Rutas 
-app.get( '/', (req, res) => {
-    res.json( {
-        ok: true,
-        msg: 'Hola mundo'
-    });
-});
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
+
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
